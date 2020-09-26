ea6d1b028cc63ddf4b4dec7f4eb319ff54fc4729
Yasuhiro Яша Asaka <yasuhiro.asaka@grauwoelfchen.net>
2020-09-26 09:50:08 +0900

    Add initial implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,43 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+#[derive(Debug, Clone)]
+pub struct Message {
+    text: String,
+    args: Vec<String>,
+}
+
+#[derive(Debug, Clone)]
+pub struct Feedback {
+    pub field: String,
+    pub messages: Vec<Message>,
+}
+
+#[derive(Debug, Clone)]
+pub struct Error(pub Vec<Feedback>);
+
+pub mod validator;
+
+#[macro_export]
+macro_rules! validate {
+    ( $( $n:expr => $v:expr => [ $( $c:expr ),* ] ),* ) => {{
+        use adequate::Feedback;
+
+        let errors = vec![$(
+            Feedback {
+                field: $n.to_string(),
+                messages: [ $( $c(&$v) ),* ]
+                    .iter()
+                    .cloned()
+                    .filter(|c| c.err())
+                    .collect::<Vec<_>>()
+            }
+        ),*]
+            .iter()
+            .cloned()
+            .filter(|f| f.messages.len() > 0 )
+            .collect::<Vec<_>>();
+        if errors.len() > 0 {
+            Err(Error(errors))
+        } else {
+            Ok(())
+        }
+    }};
 }
