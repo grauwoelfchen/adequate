5a3043fb8b5bc9c1ed33cfbb25110b9ec331e54e
Yasuhiro Яша Asaka <yasuhiro.asaka@grauwoelfchen.net>
2021-05-20 01:28:06 +0900

    Remove accidentally added dbg! macro

--- src/error.rs
+++ src/error.rs
@@ -7,7 +7,6 @@ pub struct Error(pub Vec<Feedback>);
 impl PartialEq for Error {
     fn eq(&self, other: &Self) -> bool {
         for (i, f) in self.0.iter().enumerate() {
-            dbg!(f != &other.0[i]);
             if f != &other.0[i] {
                 return false;
             }
